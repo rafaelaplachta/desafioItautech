d39d744fabad78602fd6aff8a271aa861fa3016d
Rafaela Plachta <rafaelaplachta@gmail.com>
2021-07-28 14:56:13 -0300

    ...

---
+++ script.js
@@ -0,0 +1,7 @@
+// Alert ao clicar na logo do Itaú.
+
+const logo = document.getElementById('itau')
+
+logo.addEventListener('click', function () {
+    alert("Curiosidade: 'itaú' significa pedra preta em tupi-guarani.");
+})
